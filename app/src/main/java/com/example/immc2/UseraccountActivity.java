a910b45443da756feb78285b8c39a5515074fba9
abhishekdas <abhishekdas218@gmail.com>
2020-02-18 16:58:46 +0530

    added circular imageview to user account layout

--- app/src/main/java/com/example/immc2/UseraccountActivity.java
+++ app/src/main/java/com/example/immc2/UseraccountActivity.java
@@ -4,7 +4,9 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,6 +20,7 @@ import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.HashMap;
 
+import de.hdodenhof.circleimageview.CircleImageView;
 import es.dmoral.toasty.Toasty;
 
 public class UseraccountActivity extends AppCompatActivity {
@@ -29,6 +32,9 @@ public class UseraccountActivity extends AppCompatActivity {
     EditText userzipcode;
     EditText userlandmark;
     Button saveprofile;
+
+    CircleImageView profilepic;
+
 
 
 
@@ -54,6 +60,7 @@ public class UseraccountActivity extends AppCompatActivity {
         userzipcode=findViewById(R.id.enterzipcode);
         userlandmark=findViewById(R.id.enterlandmark);
         saveprofile=findViewById(R.id.btnsave);
+        profilepic=findViewById(R.id.propicMain);
 
 
         saveprofile.setOnClickListener(new View.OnClickListener() {
@@ -109,11 +116,9 @@ public class UseraccountActivity extends AppCompatActivity {
                 }
             }
 
-
-
         });
+    }
 
 
-}
 }
 
