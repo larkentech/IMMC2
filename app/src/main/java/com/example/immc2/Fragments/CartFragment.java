fce37ebfd0736d058155765ff131af9001a57ba0
abhishekdas <abhishekdas218@gmail.com>
2020-02-18 16:46:37 +0530

    Add to CArt feature is added in IntroductionFragment.

--- app/src/main/java/com/example/immc2/Fragments/CartFragment.java
+++ app/src/main/java/com/example/immc2/Fragments/CartFragment.java
@@ -3,19 +3,46 @@ package com.example.immc2.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
 
+import com.example.immc2.AdapterClasses.CartAdapter;
+import com.example.immc2.ModalClasses.BooksModal;
 import com.example.immc2.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class CartFragment extends Fragment {
+
+    CartAdapter adapter;
+    ListView cartListView;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    Button proceedToPay;
+    FirebaseAuth mAuth;
+
+    List<String> bookId;
+    List<String> bookCategoryId;
+    List<String> bookSubCategoryId;
 
 
     public CartFragment() {
@@ -27,7 +54,60 @@ public class CartFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference().child("UserDetails").child(mAuth.getCurrentUser().getUid());
+
+        bookId = new ArrayList<>();
+        bookCategoryId = new ArrayList<>();
+        bookSubCategoryId = new ArrayList<>();
         return inflater.inflate(R.layout.fragment_cart, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.hasChild("Cart"))
+                {
+                    databaseReference.child("Cart").addChildEventListener(new ChildEventListener() {
+                        @Override
+                        public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                            bookId.add(dataSnapshot.child("BookId").getValue(String.class));
+                            bookCategoryId.add(dataSnapshot.child("BookCategory").getValue(String.class));
+                            bookSubCategoryId.add(dataSnapshot.child("BookSubCategory").getValue(String.class));
+                        }
+
+                        @Override
+                        public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                        }
+
+                        @Override
+                        public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+                        }
+
+                        @Override
+                        public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
 }
